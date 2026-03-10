28d91edc03f4bb8127362ae15b5fd4e29faadd9b
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-03-10 18:21:46 +0100

    a11y(most-used-vehicle-chart): add semantic figure, figcaption, aria-labelledby and aria-describedby to top 3 MostUsedVehicleChartComponent for screen reader support

--- src/app/features/graphics/components/most-used-vehicle-chart/most-used-vehicle-chart.ts
+++ src/app/features/graphics/components/most-used-vehicle-chart/most-used-vehicle-chart.ts
@@ -5,8 +5,6 @@ import { TimePeriod } from '../../enums/time-period.enum';
 
 import { GraphicsServices } from '../../services/graphics-services';
 import { VehicleService } from '../../../vehicle/services/vehicle-service/vehicle-service';
-
-
 @Component({
   selector: 'app-most-used-vehicle-chart',
   standalone: true,
