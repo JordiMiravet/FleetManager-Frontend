39584ad0588294bd2e3af142af0e1b0a8e7aeb76
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-05-09 19:20:28 +0200

    refactor(shared): improve ThemeService readability and naming consistency

--- src/app/shared/services/theme/theme-service.ts
+++ src/app/shared/services/theme/theme-service.ts
@@ -1,30 +1,30 @@
 import { Injectable, signal, effect } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class ThemeService {
   isDark = signal(true);
 
   constructor() {
-    const saved = localStorage.getItem('darkMode');
+    const storedDarkMode = localStorage.getItem('darkMode');
 
-    const initialValue = saved !== null 
-        ? saved === 'true' 
-        : true;
+    const initialValue =
+      storedDarkMode === null
+        ? true
+        : storedDarkMode === 'true';
 
     this.isDark.set(initialValue);
 
     effect(() => {
-      const value = this.isDark();
+      const isDark = this.isDark();
 
-      localStorage.setItem('darkMode', String(value));
-      document.body.classList.toggle('dark-mode', value);
+      localStorage.setItem('darkMode', String(isDark));
+      document.body.classList.toggle('dark-mode', isDark);
     });
   }
 
   toggle() {
-    this.isDark.update(v => !v);
+    this.isDark.update((isDark) => !isDark);
   }
 }
