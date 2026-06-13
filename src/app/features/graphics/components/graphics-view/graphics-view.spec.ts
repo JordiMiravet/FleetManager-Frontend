0846c1f82e0a36cfc01985182f650943237a9cd0
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-06-13 19:22:11 +0200

    test(graphics-view): add test structure for full component coverage (#91)

--- src/app/features/graphics/components/graphics-view/graphics-view.spec.ts
+++ src/app/features/graphics/components/graphics-view/graphics-view.spec.ts
@@ -6,7 +6,9 @@ import { provideHttpClient } from '@angular/common/http';
 import { GraphicsViewComponent } from './graphics-view';
 
 import { VehicleService } from '../../../vehicle/data-access/vehicle-service';
+import { EventService } from '../../../calendar/data-access/event-service';
 import { VehicleInterface } from '../../../vehicle/interfaces/vehicle/vehicle';
+import { TimePeriod } from '../../enums/time-period.enum';
 
 export const authMock = {
   currentUser: {
@@ -23,22 +25,25 @@ const vehicleServiceMock = {
   deleteVehicle: jasmine.createSpy('deleteVehicle')
 };
 
-describe('GraphicsView', () => {
+const eventServiceMock = {
+  loadEvents: jasmine.createSpy('loadEvents'),
+  _allEvents: jasmine.createSpy('_allEvents').and.returnValue([])
+};
+
+describe('GraphicsViewComponent', () => {
   let component: GraphicsViewComponent;
   let fixture: ComponentFixture<GraphicsViewComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        GraphicsViewComponent,
-      ], 
+      imports: [GraphicsViewComponent],
       providers: [
         provideHttpClient(),
         { provide: Auth, useValue: authMock },
-        { provide: VehicleService, useValue: vehicleServiceMock }
+        { provide: VehicleService, useValue: vehicleServiceMock },
+        { provide: EventService, useValue: eventServiceMock }
       ]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GraphicsViewComponent);
     component = fixture.componentInstance;
@@ -47,5 +52,55 @@ describe('GraphicsView', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('initial state', () => {
+    it('should initialize selectedPeriod with Month', () => {
+
+    });
+  });
+
+  describe('lifecycle', () => {
+    it('should call loadVehicles on ngAfterViewInit', () => {
+
+    });
+
+    it('should call loadEvents on ngAfterViewInit', () => {
+
+    });
+  });
+
+  describe('period changes', () => {
+    it('should change selectedPeriod to Year', () => {
+
+    });
+
+    it('should change selectedPeriod to AllTime', () => {
+
+    });
+  });
+
+  describe('rendering', () => {
+    it('should render VehicleUsageHoursChartComponent', () => {
+
+    });
+
+    it('should render MostUsedVehicleChartComponent', () => {
+
+    });
+
+    it('should render HoursByWeekdayVehicleChartComponent', () => {
+
+    });
+  });
+
+  describe('user interactions', () => {
+    it('should update selectedPeriod when clicking This Year button', () => {
+
+    });
+
+    it('should update selectedPeriod when clicking All Time button', () => {
+
+    });
   });
 });
