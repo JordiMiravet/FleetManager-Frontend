07a774cc8dfd3539f54c59cf7770544d1c0fb3dd
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-06-15 12:32:01 +0200

    test(vehicle-usage-hours-chart): add test structure (#94)

--- src/app/features/graphics/components/vehicle-usage-hours-chart/vehicle-usage-hours-chart.spec.ts
+++ src/app/features/graphics/components/vehicle-usage-hours-chart/vehicle-usage-hours-chart.spec.ts
@@ -1,11 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { Auth } from '@angular/fire/auth';
 
 import { VehicleUsageHoursChartComponent } from './vehicle-usage-hours-chart';
 
 import { GraphicsServices } from '../../data-access/graphics-services';
 import { VehicleService } from '../../../vehicle/data-access/vehicle-service';
+import { TimePeriod } from '../../enums/time-period.enum';
 
 export const authMock = {
   currentUser: {
@@ -17,27 +18,69 @@ export const authMock = {
 describe('VehicleUsageHoursChartComponent', () => {
   let component: VehicleUsageHoursChartComponent;
   let fixture: ComponentFixture<VehicleUsageHoursChartComponent>;
+  let graphicsService: GraphicsServices;
+  let vehicleService: VehicleService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        VehicleUsageHoursChartComponent,
-        HttpClientModule
-      ],
+      imports: [VehicleUsageHoursChartComponent],
       providers: [
-        GraphicsServices,
-        VehicleService,
+        provideHttpClient(),
         { provide: Auth, useValue: authMock },
+        GraphicsServices,
+        VehicleService
       ]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(VehicleUsageHoursChartComponent);
     component = fixture.componentInstance;
+    graphicsService = TestBed.inject(GraphicsServices);
+    vehicleService = TestBed.inject(VehicleService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('period input', () => {
+
+    it('should default to TimePeriod.Month');
+
+    it('should accept a different period value');
+
+  });
+
+  describe('chart creation', () => {
+
+    it('should call getVehicleUsageHours when canvas is available');
+
+    it('should not create chart if data is empty');
+
+    it('should destroy previous chart before creating a new one');
+
+    it('should not create chart if canvas is not available');
+
+  });
+
+  describe('ngOnDestroy', () => {
+
+    it('should destroy the chart on component destroy');
+
+    it('should not throw if chart was never created');
+
+  });
+
+  describe('template', () => {
+
+    it('should render the canvas element');
+
+    it('should have role="img" on the figure');
+
+    it('should have aria-labelledby pointing to the title');
+
+    it('should have aria-describedby pointing to the description');
+
+  });
+
 });
