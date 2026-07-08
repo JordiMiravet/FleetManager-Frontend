0bdae111f75b247e8fbc6bf1f5e7172533f0b08b
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-07-08 15:32:04 +0200

    refactor(map): define map feature routes (#224)

--- src/app/features/map/map.routes.ts
+++ src/app/features/map/map.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const MAP_ROUTES: Routes = [];
+export const MAP_ROUTES: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+        import('./pages/map/map-page').then(m => m.MapPageComponent)
+    }
+];
