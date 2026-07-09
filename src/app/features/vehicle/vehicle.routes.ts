8526593273ecd5370912bfcc633289dd049432d2
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-07-09 17:22:51 +0200

    refactor(vehicle): define vehicle feature routes (#223)

--- src/app/features/vehicle/vehicle.routes.ts
+++ src/app/features/vehicle/vehicle.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const VEHICLE_ROUTES: Routes = [];
+export const VEHICLE_ROUTES: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import('./pages/vehicle/vehicle-page').then(m => m.VehiclePageComponent)
+    },
+];
