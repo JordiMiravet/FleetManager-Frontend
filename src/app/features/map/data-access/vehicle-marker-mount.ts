4b5b4c12239d001dcfaba2dd2dcc6f68e7477a09
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-07-16 17:55:08 +0200

    feat(map): add createIcon method to VehicleMarkerMount (#256)

--- src/app/features/map/data-access/vehicle-marker-mount.ts
+++ src/app/features/map/data-access/vehicle-marker-mount.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import * as L from 'leaflet';
+
+const ICON_SIZE: [number, number] = [75, 75];
+const ICON_ANCHOR: [number, number] = [24, 24];
 
 @Injectable({
   providedIn: 'root',
 })
 export class VehicleMarkerMount {
+
+  createIcon(): L.DivIcon {
+    return L.divIcon({
+      className: 'vehicle-marker-host',
+      html: '',
+      iconSize: ICON_SIZE,
+      iconAnchor: ICON_ANCHOR,
+    });
+  }
+
   constructor() { }
 }
