79ff42521c7556f330ee3ec29f11efa4aa7b5b5f
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-07-16 17:48:22 +0200

    feat(map): add vehicle input to VehicleMarkerComponent (#256)

--- src/app/features/map/components/vehicle-marker/vehicle-marker.spec.ts
+++ src/app/features/map/components/vehicle-marker/vehicle-marker.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { VehicleMarker } from './vehicle-marker';
+import { VehicleMarkerComponent } from './vehicle-marker';
 
-describe('VehicleMarker', () => {
-  let component: VehicleMarker;
-  let fixture: ComponentFixture<VehicleMarker>;
+describe('VehicleMarkerComponent', () => {
+  let component: VehicleMarkerComponent;
+  let fixture: ComponentFixture<VehicleMarkerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [VehicleMarker]
+      imports: [VehicleMarkerComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(VehicleMarker);
+    fixture = TestBed.createComponent(VehicleMarkerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/features/map/components/vehicle-marker/vehicle-marker.ts
+++ src/app/features/map/components/vehicle-marker/vehicle-marker.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { VehicleInterface } from '../../../vehicle/interfaces/vehicle/vehicle';
 
 @Component({
   selector: 'app-vehicle-marker',
+  standalone: true,
   imports: [],
   templateUrl: './vehicle-marker.html',
   styleUrl: './vehicle-marker.css',
 })
-export class VehicleMarker {
+export class VehicleMarkerComponent {
+  public readonly vehicle = input.required<VehicleInterface>();
 
 }
