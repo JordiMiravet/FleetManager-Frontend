deaa2ca3c64e26ec7c1834ac9fe764d56e58ba09
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-07-26 17:53:45 +0200

    test(most-used-vehicle-chart): improve empty data assertion (#287)

--- src/app/features/graphics/components/most-used-vehicle-chart/most-used-vehicle-chart.spec.ts
+++ src/app/features/graphics/components/most-used-vehicle-chart/most-used-vehicle-chart.spec.ts
@@ -79,7 +79,7 @@ describe('MostUsedVehicleChartComponent', () => {
       component['mostUsedVehicle'] = { nativeElement: document.createElement('canvas') } as any;
       component['createMostUsedVehicleChart']();
 
-      expect(component['chart']).toBeFalsy();
+      expect(component['chart']).toBeUndefined();
     });
 
     it('should destroy previous chart before creating a new one', () => {
