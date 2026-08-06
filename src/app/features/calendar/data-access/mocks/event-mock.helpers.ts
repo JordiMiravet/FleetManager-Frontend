b8b2fadd5b178e01f7979bcaa40af3cc05977eef
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-08-06 18:02:23 +0200

    feat(calendar): implement add mock event logic (#332)

--- src/app/features/calendar/data-access/mocks/event-mock.helpers.ts
+++ src/app/features/calendar/data-access/mocks/event-mock.helpers.ts
@@ -9,8 +9,17 @@ export function getMockEventById(id: string): EventInterface | undefined {
     return MOCK_EVENTS.find(event => event._id === id);
 }
 
-export function addMockEvent(event: Omit<EventInterface, '_id'>): EventInterface {
+export function addMockEvent(
+    event: Omit<EventInterface, '_id'>
+): EventInterface {
+    const newEvent: EventInterface = {
+        ...event,
+        _id: crypto.randomUUID(),
+    };
 
+    MOCK_EVENTS.push(newEvent);
+
+    return newEvent;
 }
 
 export function updateMockEvent(updatedEvent: EventInterface): EventInterface[] {
