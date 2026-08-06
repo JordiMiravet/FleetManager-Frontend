0fdac9565c7ec1eabd052a88f14f26cddefe6feb
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-08-06 17:55:11 +0200

    chore(calendar): scaffold event mock helper functions (#332)

--- src/app/features/calendar/data-access/mocks/event-mock.helpers.ts
+++ src/app/features/calendar/data-access/mocks/event-mock.helpers.ts
@@ -1,2 +1,22 @@
 import { EventInterface } from '../../models/event';
 import { MOCK_EVENTS } from './event-data.mock';
+
+export function loadMockEvents(): EventInterface[] {
+
+}
+
+export function getMockEventById(id: string): EventInterface | undefined {
+
+}
+
+export function addMockEvent(event: Omit<EventInterface, '_id'>): EventInterface {
+
+}
+
+export function updateMockEvent(updatedEvent: EventInterface): EventInterface[] {
+
+}
+
+export function deleteMockEvent(id: string): EventInterface[] {
+
+}
