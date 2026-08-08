d0280ab6b0f9d270c9ad5a40f1eeeaa443aac439
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-08-08 19:25:19 +0200

    style(event-mock): improve test formatting (#349)

--- src/app/features/calendar/data-access/mocks/event-mock.helpers.spec.ts
+++ src/app/features/calendar/data-access/mocks/event-mock.helpers.spec.ts
@@ -2,13 +2,13 @@ import { getMockEventById, loadMockEvents } from './event-mock.helpers';
 import { MOCK_EVENTS } from './event-data.mock';
 
 describe('event-mock.helpers', () => {
-  describe('loadMockEvents', () => {
-    it('should return all mock events', () => {
-      const events = loadMockEvents();
+    describe('loadMockEvents', () => {
+        it('should return all mock events', () => {
+            const events = loadMockEvents();
 
-      expect(events).toEqual(MOCK_EVENTS);
+            expect(events).toEqual(MOCK_EVENTS);
+        });
     });
-  });
 
     describe('getMockEventById', () => {
         it('should return the event with the given id', () => {
