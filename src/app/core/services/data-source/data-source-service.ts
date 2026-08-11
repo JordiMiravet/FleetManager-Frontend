bd6d9ea8cb9dadfd65c8fe3cd61c99ce46ad478a
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-08-11 09:20:47 +0200

    feat(core): add DataSourceService as shared data source resolver (#340)

--- src/app/core/services/data-source/data-source-service.ts
+++ src/app/core/services/data-source/data-source-service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable, signal } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { DataSource } from './data-source.type';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataSourceService {
+
+  private readonly _current = signal<DataSource>(environment.dataSource);
+
+  public readonly current = this._current.asReadonly();
+
+  isMock(): boolean {
+    return this._current() === 'mock';
+  }
+
+  isApi(): boolean {
+    return this._current() === 'api';
+  }
 
 }
