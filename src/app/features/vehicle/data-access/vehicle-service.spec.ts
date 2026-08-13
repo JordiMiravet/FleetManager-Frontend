5e8f0fd514c59f404f49fb2e71260fc43f94e654
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-08-13 19:57:11 +0200

    test(vehicle-service): configure mock mode in updateVehicleLocation tests (#342)

--- src/app/features/vehicle/data-access/vehicle-service.spec.ts
+++ src/app/features/vehicle/data-access/vehicle-service.spec.ts
@@ -285,7 +285,7 @@ describe('VehicleService', () => {
   describe('updateVehicleLocation (mock)', () => {
 
     beforeEach(() => {
-      (service as any).useMock = true;
+      dataSourceServiceMock.isMock.and.returnValue(true);
       service.vehicles.set([ferrariMock]);
     });
 
