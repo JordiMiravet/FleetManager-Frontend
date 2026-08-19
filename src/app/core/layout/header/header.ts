91e01f231619cded4b09179df546fcb7b4243e10
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-08-19 17:20:14 +0200

    feat(layout): integrate NotificationBellComponent into header (#183)

--- src/app/core/layout/header/header.ts
+++ src/app/core/layout/header/header.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from "../navigation/navigation";
 import { AuthActionsComponent } from "../auth-actions/auth-actions";
+import { NotificationBellComponent } from '../notification-bell/notification-bell';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     NavigationComponent, 
-    AuthActionsComponent
+    AuthActionsComponent,
+    NotificationBellComponent
   ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
