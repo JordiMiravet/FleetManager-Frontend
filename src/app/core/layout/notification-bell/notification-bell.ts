aabaaa05287edf23563ab5c259414af3dca56ee4
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-08-20 11:58:38 +0200

    feat(notification-bell): inject InvitationService into NotificationBellComponent (#184)

--- src/app/core/layout/notification-bell/notification-bell.ts
+++ src/app/core/layout/notification-bell/notification-bell.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { LayoutMessagesService } from '../i18n/layout-messages-service';
+import { InvitationService } from '../../../features/invitations/data-access/invitation-service';
 
 @Component({
   selector: 'app-notification-bell',
@@ -10,6 +11,9 @@ import { LayoutMessagesService } from '../i18n/layout-messages-service';
 export class NotificationBellComponent {
 
   private readonly messagesService = inject(LayoutMessagesService);
+  private readonly invitationService = inject(InvitationService);
 
   public readonly notificationBellMsg = this.messagesService.notificationBell;
+  public readonly pendingCount = this.invitationService.pendingCount;
+
 }
