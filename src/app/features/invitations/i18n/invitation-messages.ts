a37d77835892ac7249552aa47d19a6a5009d986c
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-08-22 16:31:12 +0200

    feat(invitation-messages): add invitation card labels, buttons and aria messages in i18n (#186)

--- src/app/features/invitations/i18n/invitation-messages.ts
+++ src/app/features/invitations/i18n/invitation-messages.ts
@@ -5,4 +5,18 @@ import { Injectable } from '@angular/core';
 })
 export class InvitationMessagesService {
 
+  readonly card = {
+    labels: {
+        owner: 'Shared by'
+    },
+    buttons: {
+        accept: 'Accept',
+        decline: 'Decline'
+    },
+    aria: {
+        accept: (vehicleName: string) => `Accept invitation for ${vehicleName}`,
+        decline: (vehicleName: string) => `Decline invitation for ${vehicleName}`
+    }
+  };
+
 }
