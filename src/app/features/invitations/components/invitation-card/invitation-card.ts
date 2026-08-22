a273a444109f1a4286e076fbc6497dd0cd2b6ac2
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-08-22 16:43:50 +0200

    refactor(invitation-card): make InvitationCardComponent standalone (#186)

--- src/app/features/invitations/components/invitation-card/invitation-card.ts
+++ src/app/features/invitations/components/invitation-card/invitation-card.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-invitation-card',
+  standalone: true,
   imports: [],
   templateUrl: './invitation-card.html',
   styleUrl: './invitation-card.scss',
