c2df6a551bcce758a46e08b73daad1cfa82fedeb
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-08-22 16:43:16 +0200

    refactor(invitation-card): rename InvitationCard to InvitationCardComponent (#186)

--- src/app/features/invitations/components/invitation-card/invitation-card.ts
+++ src/app/features/invitations/components/invitation-card/invitation-card.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './invitation-card.html',
   styleUrl: './invitation-card.scss',
 })
-export class InvitationCard {
+export class InvitationCardComponent {
 
 }
