94cb32a8f03544ee589cbed9d15f08aeaabf4108
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-08-26 19:06:38 +0200

    feat(vehicle-access): inject vehicle and invitation services (#190)

--- src/app/core/services/vehicle-access/vehicle-access-service.ts
+++ src/app/core/services/vehicle-access/vehicle-access-service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+
+import { VehicleService } from '../../../features/vehicle/data-access/vehicle-service';
+import { InvitationService } from '../../../features/invitations/data-access/invitation-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class VehicleAccessService {
-  
+
+  private readonly vehicleService = inject(VehicleService);
+  private readonly invitationService = inject(InvitationService);
+
 }
