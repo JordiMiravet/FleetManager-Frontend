6452ae6aef4540bea764f843ee19f672d23c8211
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-08-26 19:09:24 +0200

    test(vehicle-access): inject vehicle and invitation services in tests (#190)

--- src/app/core/services/vehicle-access/vehicle-access-service.spec.ts
+++ src/app/core/services/vehicle-access/vehicle-access-service.spec.ts
@@ -1,13 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { VehicleAccessService } from './vehicle-access-service';
+import { VehicleService } from '../../../features/vehicle/data-access/vehicle-service';
+import { InvitationService } from '../../../features/invitations/data-access/invitation-service';
 
 describe('VehicleAccessService', () => {
   let service: VehicleAccessService;
+  let vehicleService: VehicleService;
+  let invitationService: InvitationService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
+    
     service = TestBed.inject(VehicleAccessService);
+    vehicleService = TestBed.inject(VehicleService);
+    invitationService = TestBed.inject(InvitationService);
   });
 
   it('should be created', () => {
