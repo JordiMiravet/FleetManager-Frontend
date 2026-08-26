59fcfffbded8f9ed3cb3ff315fbe72e39d48382b
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-08-26 19:11:01 +0200

    test(vehicle-access): add test data builders for vehicles and invitations (#190)

--- src/app/core/services/vehicle-access/vehicle-access-service.spec.ts
+++ src/app/core/services/vehicle-access/vehicle-access-service.spec.ts
@@ -3,15 +3,36 @@ import { TestBed } from '@angular/core/testing';
 import { VehicleAccessService } from './vehicle-access-service';
 import { VehicleService } from '../../../features/vehicle/data-access/vehicle-service';
 import { InvitationService } from '../../../features/invitations/data-access/invitation-service';
+import { VehicleInterface } from '../../../features/vehicle/models/vehicle';
+import { InvitationStatus } from '../../../features/invitations/enums/invitation-status.enum';
+import { VehicleInvitation } from '../../../features/invitations/models/invitation';
 
 describe('VehicleAccessService', () => {
   let service: VehicleAccessService;
   let vehicleService: VehicleService;
   let invitationService: InvitationService;
 
+  const buildVehicle = (id: string): VehicleInterface => ({
+    _id: id,
+    name: `Vehicle ${id}`,
+    model: 'Model',
+    plate: `PLATE-${id}`,
+  });
+
+  const buildInvitation = (vehicleId: string, status: InvitationStatus): VehicleInvitation => ({
+    _id: crypto.randomUUID(),
+    vehicleId,
+    vehicleName: `Vehicle ${vehicleId}`,
+    ownerId: 'owner-1',
+    ownerEmail: 'owner1@fleetmanager.dev',
+    invitedEmail: 'you@fleetmanager.dev',
+    status,
+    invitedAt: '2026-08-10',
+  });
+  
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    
+
     service = TestBed.inject(VehicleAccessService);
     vehicleService = TestBed.inject(VehicleService);
     invitationService = TestBed.inject(InvitationService);
