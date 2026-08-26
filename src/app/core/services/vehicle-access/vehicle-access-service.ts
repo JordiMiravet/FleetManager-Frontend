9b91721e32af027b3e01def50d8b9c0e6aa75e1c
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-08-26 19:07:38 +0200

    feat(vehicle-access): filter visible vehicles by invitation status (#190)

--- src/app/core/services/vehicle-access/vehicle-access-service.ts
+++ src/app/core/services/vehicle-access/vehicle-access-service.ts
@@ -1,7 +1,8 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, computed, inject } from '@angular/core';
 
 import { VehicleService } from '../../../features/vehicle/data-access/vehicle-service';
 import { InvitationService } from '../../../features/invitations/data-access/invitation-service';
+import { InvitationStatus } from '../../../features/invitations/enums/invitation-status.enum';
 
 @Injectable({
   providedIn: 'root',
@@ -10,5 +11,18 @@ export class VehicleAccessService {
 
   private readonly vehicleService = inject(VehicleService);
   private readonly invitationService = inject(InvitationService);
+
+  public readonly visibleVehicles = computed(() => {
+    const vehicles = this.vehicleService.vehicles();
+    const invitations = this.invitationService.invitations();
+
+    return vehicles.filter(vehicle => {
+      const invitation = invitations.find(inv => inv.vehicleId === vehicle._id);
+
+      if (!invitation) return true;
+
+      return invitation.status === InvitationStatus.Accepted;
+    });
+  });
 
 }
