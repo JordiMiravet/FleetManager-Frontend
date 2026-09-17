cb34bb805445df479dbfdaf2e9b0e630495226b1
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-09-17 18:40:03 +0200

    style(most-used-vehicle-chart): rename variable for better readability (#431)

--- src/app/features/graphics/components/most-used-vehicle-chart/most-used-vehicle-chart.spec.ts
+++ src/app/features/graphics/components/most-used-vehicle-chart/most-used-vehicle-chart.spec.ts
@@ -157,9 +157,9 @@ describe('MostUsedVehicleChartComponent', () => {
 
     it('should have aria-describedby pointing to the description', () => {
       const figure = getFigure();
-      const desc = getDescription();
+      const description = getDescription();
 
-      expect(figure.getAttribute('aria-describedby')).toBe(desc.getAttribute('id'));
+      expect(figure.getAttribute('aria-describedby')).toBe(description.getAttribute('id'));
     });
 
   });
