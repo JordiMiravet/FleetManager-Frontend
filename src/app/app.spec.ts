3a29b5ab17953d219dad8d0ad385fa63a5b131f6
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-09-19 17:21:06 +0200

    refactor(app): verify app content container class (#436)

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -42,7 +42,7 @@ describe('App', () => {
     });
 
     it('should render the app content container', () => {
-      expect(getAppContent()).toBeTruthy();
+      expect(getAppContent()?.classList.contains('app-content')).toBeTrue();
     });
 
     it('should render the router outlet inside the app content container', () => {
