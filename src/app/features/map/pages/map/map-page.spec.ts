98dcee9834b9458ada3a48df062554be554c5c1f
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-09-21 18:17:58 +0200

    refactor(map-page): simplify test suite structure (#433)

--- src/app/features/map/pages/map/map-page.spec.ts
+++ src/app/features/map/pages/map/map-page.spec.ts
@@ -22,9 +22,7 @@ describe('MapPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        MapPageComponent, 
-      ],
+      imports: [ MapPageComponent ],
       providers: [
         { provide: Auth, useValue: authMock },
         provideHttpClient(),
@@ -37,12 +35,8 @@ describe('MapPageComponent', () => {
     fixture.detectChanges();
   });
 
-  describe('Component creation', () => {
-
-    it('should create', () => {
-      expect(component).toBeTruthy();
-    });
-
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 
   describe('child components rendering', () => {
