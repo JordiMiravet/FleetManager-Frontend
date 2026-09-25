71728f1271f41353f3b6322f7d19713514ad85aa
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-09-25 16:19:55 +0200

    test(most-used-vehicle-chart): add period description test cases (#423)

--- src/app/features/graphics/components/most-used-vehicle-chart/most-used-vehicle-chart.spec.ts
+++ src/app/features/graphics/components/most-used-vehicle-chart/most-used-vehicle-chart.spec.ts
@@ -60,6 +60,22 @@ describe('MostUsedVehicleChartComponent', () => {
     });
 
   });
+  
+  describe('period description', () => {
+
+    it('should return current month for TimePeriod.Month', () => {
+
+    });
+
+    it('should return current year for TimePeriod.Year', () => {
+
+    });
+
+    it('should return all time for TimePeriod.AllTime', () => {
+
+    });
+
+  });
 
   describe('chart creation', () => {
 
